7d89626c47ba39f0b6962ebee9fede97a9227458
cory-young <cory-young@users.noreply.github.com>
2024-01-26 11:52:32 -0800

    Updated solutions
    Updated Leetcode solutions with links to questions

--- SQL/Leetcode-problems/1211_Queries_Quality_and_Percentage.sql
+++ SQL/Leetcode-problems/1211_Queries_Quality_and_Percentage.sql
@@ -1,4 +1,8 @@
--- sum/count option this does not exclude null values
+/* Write a solution to find each query_name, the quality and poor_query_percentage.
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+Return the result table in any order. */
+
+-- sum/count option is accepted but will not exclude null values
 SELECT query_name,
        Round(Sum(rating / position) / Count(*), 2) AS quality,
        Round(Sum(CASE
@@ -8,9 +12,11 @@ SELECT query_name,
 FROM   queries
 GROUP  BY query_name;
 
---use round aggregate excludes nulls for you
+--Better Solution: round aggregate to exclude nulls
 SELECT query_name,
        Round(Avg(rating / position), 2) AS quality,
        Round(Avg(rating < 3) * 100, 2)  AS poor_query_percentage
 FROM   queries
 GROUP  BY query_name; 
+
+-- Link: https://leetcode.com/problems/queries-quality-and-percentage/description/
--- SQL/Leetcode-problems/183_Customers_Who_Never_Order.sql
+++ SQL/Leetcode-problems/183_Customers_Who_Never_Order.sql
@@ -1,13 +1,17 @@
---183 Customers Who Never Order
---JOIN
+/* Write a solution to find all customers who never order anything.
+Return the result table in any order. */
+
+--Solution 1: JOIN
 SELECT name AS customers
 FROM   customers AS c
        LEFT JOIN orders AS o
               ON c.id = o.customerid
 WHERE  customerid IS NULL;
 
---Sub Query
+--Solution 2: Sub Query
 SELECT name AS Customer
 FROM   customers
 WHERE  id NOT IN (SELECT customerid
                   FROM   orders); 
+
+-- Link: https://leetcode.com/problems/customers-who-never-order/description/
